a415cdb6f0c22657a87e092682cdb90811024be3
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-03-29 01:06:31 -0600

    Delete users

--- main.js
+++ main.js
@@ -21,18 +21,29 @@ function createUsers() {
     });
 }
 
-let table=document.getElementById('display');
+let table = document.getElementById('display');
 db.collection("users").onSnapshot((querySnapshot) => {
-  table.innerHTML='';
+  table.innerHTML = '';
   querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data().first}`);
-      table.innerHTML+=`
+    console.log(`${doc.id} => ${doc.data().first}`);
+    table.innerHTML += `
       <tr>
         <th scope="row">${doc.id}</th>
         <td>${doc.data().first}</td>
         <td>${doc.data().last}</td>
         <td>${doc.data().born}</td>
+        <td><button class="btn btn-danger" onclick="deleteUsers('${doc.id}')">Delete</button></td>
+        <td><button class="btn btn-warning">Update</button></td>
       </tr>
       `
   });
 });
+
+function deleteUsers(user){
+  db.collection("users").doc(user).delete().then(() => {
+    console.log("Document successfully deleted!");
+  }).catch((error) => {
+    console.error("Error removing document: ", error);
+  });
+}
+
