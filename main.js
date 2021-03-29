ba5b6d96fdffd7c38b1dde7f74e8996a0f75278b
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-03-29 00:12:44 -0600

    Create users

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyAPAtU_DhkzZ6TK5jJmTtLKon5w-cFM0g0",
-    authDomain: "jsvanillacrud.firebaseapp.com",
-    projectId: "jsvanillacrud",
+  apiKey: "AIzaSyAPAtU_DhkzZ6TK5jJmTtLKon5w-cFM0g0",
+  authDomain: "jsvanillacrud.firebaseapp.com",
+  projectId: "jsvanillacrud",
 });
 
 let db = firebase.firestore();
 
-db.collection("users").add({
-    first: "Gloria",
-    last: "Figueroa",
-    born: 1997
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
-
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
+function saveUsers() {
+  db.collection("users").add({
+    first: document.getElementById('firt-name').value,
+    last: document.getElementById('last-name').value,
+    born: document.getElementById('birth-date').value
+  })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+      document.getElementById("user-data").reset();
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
     });
-});
+}
