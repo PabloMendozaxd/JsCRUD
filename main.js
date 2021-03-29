91cbe1d8339b6c509e580a50ea80a49b7914fef5
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-03-29 10:34:29 -0600

    Update users

--- main.js
+++ main.js
@@ -33,17 +33,42 @@ db.collection("users").onSnapshot((querySnapshot) => {
         <td>${doc.data().last}</td>
         <td>${doc.data().born}</td>
         <td><button class="btn btn-danger" onclick="deleteUsers('${doc.id}')">Delete</button></td>
-        <td><button class="btn btn-warning">Update</button></td>
+        <td><button class="btn btn-warning" onclick="updateUsers('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')">Update</button></td>
       </tr>
       `
   });
 });
 
-function deleteUsers(user){
+function deleteUsers(user) {
   db.collection("users").doc(user).delete().then(() => {
     console.log("Document successfully deleted!");
   }).catch((error) => {
     console.error("Error removing document: ", error);
   });
 }
+function updateUsers(user,newfirstName,newlastName,newbirthDate) {
+  document.getElementById('firt-name').value=newfirstName;
+  document.getElementById('last-name').value=newlastName;
+  document.getElementById('birth-date').value=newbirthDate;
+  let updateButton = document.getElementById('mainButton');
+  updateButton.innerHTML = 'Update';
+  updateButton.onclick = () => {
+    var washingtonRef = db.collection("users").doc(user);
 
+    // Set the "capital" field of the city 'DC'
+    return washingtonRef.update({
+      first: document.getElementById('firt-name').value,
+      last: document.getElementById('last-name').value,
+      born: document.getElementById('birth-date').value
+    })
+      .then(() => {
+        console.log("Document successfully updated!");
+        document.getElementById("user-data").reset();
+        updateButton.innerHTML = 'Submit';
+      })
+      .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
+  }
+}
